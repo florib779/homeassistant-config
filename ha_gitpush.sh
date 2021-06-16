19f8a800d1106f1f8622e0dd10e6129d6e8ebe92
root <root@core-ssh.local.hass.io>
2021-06-16 14:36:48 +0200

    config files on 16-06-2021 14:36:48

---
+++ ha_gitpush.sh
@@ -0,0 +1,12 @@
+# Go to /config folder or 
+# Change this to your Home Assistant config folder if it is different
+cd /config
+
+# Add all files to the repository with respect to .gitignore rules
+git add .
+
+# Commit changes with message with current date stamp
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+
+# Push changes towards GitHub
+git push -u origin master
