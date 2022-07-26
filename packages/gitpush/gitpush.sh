7fd45af1a4d7dff532588eff8961050971cb79aa
root <root@a0d7b954-ssh.local.hass.io>
2022-07-26 21:34:02 +0200

    config files on 26-07-2022 21:34:02

---
+++ packages/gitpush/gitpush.sh
@@ -0,0 +1,14 @@
+# This is a script from https://peyanski.com/automatic-home-assistant-backup-to-github/
+
+# Go to /config folder or 
+# Change this to your Home Assistant config folder if it is different
+cd /config
+
+# Add all files to the repository with respect to .gitignore rules
+git add .
+
+# Commit changes with message with current date stamp
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+
+# Push changes towards GitHub
+git push -u origin master
